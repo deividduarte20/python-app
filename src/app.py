8275a71f42aea122edc161a682f8a191059f9101
Deivid Duarte <deividdua@gmail.com>
2025-11-10 19:49:11 -0300

    fix: pipeline

--- src/app.py
+++ src/app.py
@@ -5,22 +5,16 @@ import socket
 app = Flask(__name__)
 
 @app.route('/api/v1/details')
-
 def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p %S on %B %d, %Y"),
-        'hostname': socket.gethostname()
-        'message': 'You are doing great, human!#'
+        'hostname': socket.gethostname(),
+        'message': 'You are doing great, human!'
     })
 
 @app.route('/api/v1/healthz')
-
 def health():
-    return jsonify({
-        'status': 'up'}), 200
+    return jsonify({'status': 'up'}), 200
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
-
-#'/api/v1/details'
-#'/api/v1/healthzfrom'
